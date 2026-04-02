ac25b7dd433465a156bfdd0c5d7bb288fcad511d
Ivan Cézanne Seara <ivancezanne@gmail.com>
2026-04-02 12:08:26 -0300

    feat: create class for file handling

--- run.php
+++ run.php
@@ -2,4 +2,14 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+use Minesweeper\File;
+
 echo "Minesweeper is running!" . PHP_EOL;
+
+$filePath = $argv[1];
+
+$file = new File($filePath);
+
+echo $file->content(). PHP_EOL;
+
+echo "Minesweeper has ended gracefully!" . PHP_EOL;
---
+++ src/File.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Minesweeper;
+
+class File {
+    private string $content;
+
+    public function __construct(string $filePath)
+    {
+        if (!file_exists($filePath)) {
+            throw new \Exception(sprintf('File not found at path: %s', $filePath));
+        }
+
+        $content = file_get_contents($filePath);
+
+        if ($content === false) {
+            throw new \Exception(sprintf('Error reading file at path: %s', $filePath));
+        }
+
+        $this->content = $content;
+    }
+
+    public function content(): string {
+        return $this->content;
+    }
+
+    public function line(int $index = 0): string
+    {
+        $parts = explode("\n", $this->content);
+        
+        return $parts[$index];
+    }
+}
